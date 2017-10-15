07ef756d6984f25d294ce4e758e8a671942581fa
Anderson Pontes Batalha <andersonpbatalha@gmail.com>
2017-10-15 15:33:20 -0200

    Funcionalidade adicionada

--- teste-github.py
+++ teste-github.py
@@ -2,3 +2,15 @@
 # -*- coding: utf-8 -*-
 
 print ("Este é um teste.")
+
+def operacoes(tipo, n1, n2):
+	if tipo == "soma":
+		return "Soma:\t %d + %d = %d" %(n1,n2,n1+n2)
+	elif tipo == "sub":
+		return "Subtração:\t %d + %d = %d" %(n1,n2,n1-n2)
+	elif tipo == "div":
+		return "Divisão:\t %d + %d = %d" %(n1,n2,(n1+0.0)/n2)
+	elif tipo == "mult":
+		return "Mutliplicação:\t %d + %d = %d" %(n1,n2,n1*n2)
+
+print operacoes("div", 2, 10)
